2096afea0f16b195f17f05172e94335db41744c5
Cheng Weixuan <cheng_weixuan@mom.gov.sg>
2023-09-26 13:54:35 +0800

    Add yaml files

---
+++ commands.bash
@@ -0,0 +1,9 @@
+# Create network
+docker network create mynet
+
+# docker create volume for db
+docker volume create data-vol
+
+#Start the mydb container
+docker container run -d --name mydb --network mynet --mount type=volume,src=data-vol dst=/var/lib/mysql
+
