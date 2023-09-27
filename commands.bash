53a9cbd4132b6e4e072f9eee4f656811a300ca8e
Cheng Weixuan <cheng_weixuan@mom.gov.sg>
2023-09-27 16:06:46 +0800

    add workshop 3 yaml files

--- commands.bash
+++ commands.bash
@@ -7,3 +7,6 @@ docker volume create data-vol
 #Start the mydb container
 docker container run -d --name mydb --network mynet --mount type=volume,src=data-vol dst=/var/lib/mysql
 
+#gget 
+
+k get svc -ningress-nginx 
